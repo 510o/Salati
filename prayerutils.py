e8846d5201a75b6e9e61a5d10cbec8555d2cedd8
510o <1510oxzso@gmail.com>
2025-03-12 17:50:10 +0300

    Fix platform diversity

--- prayerutils.py
+++ prayerutils.py
@@ -24,7 +24,7 @@ def format_time(time, format_type: int = None) -> str:
             minutes, seconds = divmod(rem, 60)
         if format_type == 12:
             if __import__('platform').system() == 'Linux': return f"{"ﺹ" if hours < 12 else "ﻡ"} {hours % 12 or 12}:{minutes:02}:{seconds:02}".rstrip(":00")
-            return f"{hours % 12 or 12}:{minutes:02}:{seconds:02} {"ﺹ" if hours < 12 else "ﻡ"}".replace(":00 ", ' ')
+            return f"{hours % 12 or 12}:{minutes:02}:{seconds:02} {"AM" if hours < 12 else "PM"}".replace(":00 ", ' ')
         elif format_type == 24: return f"{hours}:{minutes:02}:{seconds:02}".lstrip("00").lstrip(":")
         elif format_type == 60: return f"{minutes + hours*60}:{seconds:02}"
         elif format_type == 3600: return str(total_seconds)
